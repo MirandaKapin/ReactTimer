2c9e77f2eb53dfa273ca49ef38c6bbd0bc31e763
Miranda Kapin <mirandakapin@gmail.com>
2016-07-31 20:31:31 -0700

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<div>
+					<p>Countdown.jsx Rendered</p>
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = Countdown;
